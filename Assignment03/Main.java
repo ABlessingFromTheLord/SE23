923e9feee7ea158645d4cf3ae651dce7925af3ec
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-11-07 11:45:58 +0100

    completed task 1 part 1 and 2

--- Assignment03/Main.java
+++ Assignment03/Main.java
@@ -1,7 +1,33 @@
 package Assignment03;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int MAX = 1000;
+        int factor = 623;
+        int increment = 525;
+        int start = 157;
+        int actualRandom;
+
+        while (true) {
+            // Version 1 cannot be implemented because we lack of control features
+            // such as branching, loops or goto statement to result of evaluated conditions
+
+            System.out.println("Random seed the generator? (y/n)");
+            Scanner sc1 = new Scanner(System.in);
+            char c = sc1.next().charAt(0);
+
+            //random number is generated using user specified seed value
+            if(c == 'y' || c == 'Y'){
+                System.out.println("Please put in a seed value: ");
+                int temp = sc1.nextInt();
+                actualRandom = ((factor * temp) + increment) % MAX;
+                System.out.println("Random number: " + actualRandom);
+            }
+
+            else{ break; }
+
+        }
     }
 }
