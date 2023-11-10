ef2b1a713e59bbffd248e597900e9c281dc42bed
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-11-10 19:29:51 +0100

    removed special class Entrance

--- Assignment04/Apartment.java
+++ Assignment04/Apartment.java
@@ -1,19 +1,26 @@
 package Assignment04;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Apartment {
     // Fields
+    private Stack<Room> tour;
+    private Room currentRoom;
     private ArrayList<Room> rooms;
 
     // Constructor
     public Apartment(){
         rooms = new ArrayList<>();
+        tour = new Stack<>();
+
+        // Begin in hallway
+        System.out.println("You are in the Hallway");
     }
 
     // Methods
     public void enter(){
-
+        System.out.println("You are in the");
     }
 
     public void addNewRoom(Room room){
