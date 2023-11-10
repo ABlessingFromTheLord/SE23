cdbcb4f6a4573a6b1844e67b53fddeb3810b2046
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-11-10 19:42:35 +0100

    impelemented entering apartment

--- Assignment04/Apartment.java
+++ Assignment04/Apartment.java
@@ -5,25 +5,45 @@ import java.util.Stack;
 
 public class Apartment {
     // Fields
+    private String apartmentName;
     private Stack<Room> tour;
     private Room currentRoom;
     private ArrayList<Room> rooms;
 
     // Constructor
-    public Apartment(){
+    public Apartment(String Name){
         rooms = new ArrayList<>();
         tour = new Stack<>();
 
         // Begin in hallway
-        System.out.println("You are in the Hallway");
+        Room entrance = new Room("Hallway", false);
+        rooms.add(entrance);
+        currentRoom = entrance;
+        tour.push(entrance);
+
+        System.out.println("Welcome to" +  this.getApartmentName() + " apartment");
     }
 
     // Methods
+    // Getters
+    public String getApartmentName(){
+        return this.apartmentName;
+    }
+
     public void enter(){
-        System.out.println("You are in the");
+        System.out.println("You are in the " + currentRoom);
+        System.out.println("Choose an action: ");
+    }
+
+    public void leaveRoom(){
+        System.out.println("You left the " +currentRoom.getName());
+        tour.pop();
+        currentRoom = tour.peek();
+        this.enter();
     }
 
     public void addNewRoom(Room room){
         rooms.add(room);
     }
+
 }
