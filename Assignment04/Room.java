796c073f5ae216414027e596d597b2b0cff23c8d
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-11-10 20:00:59 +0100

    added action choosing fuctionality

--- Assignment04/Room.java
+++ Assignment04/Room.java
@@ -1,14 +1,22 @@
 package Assignment04;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Room {
     // Fields
     private String name;
     private boolean lighting;
+    private Scanner sc;
+    Iterable<String> actions;
 
     // Constructor
     public Room(String Name, boolean Lighting){
         this.name = Name;
         this.lighting = Lighting;
+
+        sc = new Scanner(System.in);
+        this.init();
     }
 
     // Methods
@@ -17,11 +25,31 @@ public class Room {
         return this.name;
     }
 
+    public void init(){
+        String[] actionsArray = new String[3];
+        actionsArray[0] = "Switch Light";
+        actionsArray[1] = "Enter neighboring room";
+        actionsArray[2] = "leave room";
+        actions = Arrays.asList(actionsArray);
+    }
+
     public void enter(){
         System.out.println("You are in the " + this.name);
+        System.out.println("Choose action from the following");
+
+        int i = 0;
+        for (String s: actions){
+            System.out.println(i + " " + s);
+        }
     }
 
     public void switchLight(){
         lighting = !lighting;
+    }
+
+    public void readAction(){
+        if(sc.nextInt() == 0){
+
+        }
     }
 }
