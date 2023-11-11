1db24287d928bffb4dd9fe8c62c338ccc5a15909
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-11-11 12:21:41 +0100

    refactored living room home theater system

--- Assignment04/LivingRoom.java
+++ Assignment04/LivingRoom.java
@@ -22,37 +22,29 @@ public class LivingRoom extends Room{
     // Methods
     public void init(){
         String[] actionsArray = new String[5];
-        actionsArray[0] = "Switch tv";
-        actionsArray[1] = "Switch lighting";
-        actionsArray[2] = "Play Home theater music";
-        actionsArray[3] = "Enter neighboring room";
-        actionsArray[4] = "Leave room";
+        actionsArray[0] = "Switch TV";
+        actionsArray[1] = "Switch Lighting";
+        actionsArray[2] = "Play Home Theater System";
+        actionsArray[3] = "Enter Neighboring Room";
+        actionsArray[4] = "Leave Room";
         actions = Arrays.asList(actionsArray);
-    }
-
-    public void enter(){
-        int i = 1;
-        for (String s: actions){
-            System.out.println(i + " for " + s);
-            i++;
-        }
     }
 
     public void switchTV(){
         tv = !tv;
     }
 
-    public void playHomeTheaterMusic() throws InterruptedException {
+    public void playHomeTheaterSystem() throws InterruptedException {
         System.out.println("Never gonna give you up");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         System.out.println("Never gonna let you down");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         System.out.println("Never gonna run around and desert you");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         System.out.println("Never gonna make you cry");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         System.out.println("Never gonna say goodbye");
-        Thread.sleep(1000);
+        Thread.sleep(500);
         System.out.println("Never gonna tell a lie to hurt you");
     }
 }
